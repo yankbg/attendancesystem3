bdf65584b34484ecf5c98a8c9e1458145f94a7a5
YANNICK kabunga <yankbg2@gmail.com>
2025-06-30 18:53:46 +0300

    Update check_student.js

--- api/check_student.js
+++ api/check_student.js
@@ -5,7 +5,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 3306,
+  port: 13662,
 };
 
 module.exports = async (req, res) => {
