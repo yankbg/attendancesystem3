eabd1eceae7249204b41339cde0bc16fac459a21
YANNICK kabunga <yankbg2@gmail.com>
2025-06-30 14:35:33 +0300

    Update upload_student.js
    add cors

--- api/upload_student.js
+++ api/upload_student.js
@@ -21,6 +21,15 @@ const dbConfig = {
 };
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   if (req.method !== 'POST') {
     res.status(405).json({ status: 'error', message: 'Only POST requests are allowed' });
     return;
